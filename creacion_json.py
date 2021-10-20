12ab4bd71ac0d8e7e484cc084baa3682dd400ceb
Javier <100429142@alumnos.uc3m.es>
2021-10-20 18:47:00 +0200

    Archivo para crear los json de los usuarios

--- creacion_json.py
+++ creacion_json.py
@@ -1,1 +1,43 @@
-#ARCHIVO DONDE SE CREAN LOS JSON
+"""En este archivo se crea el Json users_data, que contiene la información de todos los usuarios registrados"""
+import json
+import os
+
+name = input("Input your name: ")
+last_name = input("Input your last name: ")
+number = input("Input your phone number: ")
+money = input("Input the max amount amount of money you want to use in our app: ")
+
+
+def users_data():
+    dat = {'Users': []}
+
+    dat['Users'].append({
+        'First_name': name,
+        'Last_name': last_name,
+        'Number': number,
+        'Money': money
+    })
+
+    return dat
+
+
+def create_json():
+    if os.path.exists('users_data.json'):
+        dats = json.load(open('users_data.json'))
+
+        dats['Users'].append({
+            'First_name': name,
+            'Last_name': last_name,
+            'Number': number,
+            'Money': money
+        })
+
+        with open('users_data.json', 'w') as file:
+            json.dump(dats, file, indent=1)
+
+    else:
+        with open('users_data.json', 'w') as file:
+            json.dump(users_data(), file, indent=1)
+
+
+create_json()
