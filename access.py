c74cd8464ee6004ec1548971fe0a324f99bd751f
JavierMartinezCasas <100429142@alumnos.uc3m.es>
2021-12-04 13:19:03 +0100

    Firma digital

--- access.py
+++ access.py
@@ -2,6 +2,10 @@ import json
 from checking_users import checking_users
 from json_atributos import json_atributos
 from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import hashes
 
 
 def access(i):
@@ -34,15 +38,27 @@ def send_money(i):
         if money > int(i['Money']):
             print('You do not have enough money to perform this transfer')
             access(i)
-        elif money >= 500:
-            certificateTrans(money, i)
+
         else:
-            key = Fernet.generate_key()
-            f = Fernet(key)
-            money = str(money)
-            money = money.encode()
-            token = f.encrypt(money)
-            receive_money(sender, receiver, token, money, key, f, i)
+            if money >= 500:
+                if certificateTrans(money, i) is True:
+                    key = Fernet.generate_key()
+                    f = Fernet(key)
+                    money = str(money)
+                    money = money.encode()
+                    token = f.encrypt(money)
+                    receive_money(sender, receiver, token, money, key, f, i)
+
+                else:
+                    return
+
+            else:
+                key = Fernet.generate_key()
+                f = Fernet(key)
+                money = str(money)
+                money = money.encode()
+                token = f.encrypt(money)
+                receive_money(sender, receiver, token, money, key, f, i)
 
     else:
         print('There is not any user with that phone number, please, try again')
@@ -95,4 +111,30 @@ def account_update(sender, receiver, money, i):
 
 
 def certificateTrans(money, i):
-    return
+    message = "I am sending money€ to number"
+
+    private_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048,
+        backend=default_backend()
+    )
+
+    signature = private_key.sign(
+        message,
+        padding.PSS(
+            mgf=padding.MGF1(hashes.SHA256()),
+            salt_length=padding.PSS.MAX_LENGTH
+        ),
+        hashes.SHA256()
+    )
+
+    public_key = private_key.public_key()
+    public_key.verify(
+        signature,
+        message,
+        padding.PSS(
+            mgf=padding.MGF1(hashes.SHA256()),
+            salt_length=padding.PSS.MAX_LENGTH
+        ),
+        hashes.SHA256()
+    )
